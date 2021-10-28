26d98621f69427264899c0d44cc5e76226516c24
aditipolkam <aditipolkam@gmail.com>
2021-10-28 22:30:00 +0530

    ADDED basic to-do functionality

---
+++ src/App.svelte
@@ -0,0 +1,37 @@
+<script>
+	let todos = [
+		{done:false, text:'Make a todo app.'}
+	];
+	function add(){
+		todos = todos.concat({done:false, text:''});
+	}
+	function clear(){
+		todos = todos.filter(t=>!t.done);
+	}
+	$: remaining = todos.filter(t=>!t.done).length;
+</script>
+
+<main>
+	<h1>Todos</h1>
+	{#each todos as todo}
+		<div class:done = {todo.done}>
+			<input type=checkbox bind:checked={todo.done}>
+			<input placeholder="Enter task..." value={todo.text}>
+		</div>
+	{/each}
+	<p>{remaining} remaining.</p>
+	<button on:click={add}>Add new item</button>
+	<button on:click={clear}>Clear completed</button>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+	.done{
+		opacity:0.4;
+	}
+</style>
